cf7d4be4497beb597cd751589515ddb24f9d8222
Aleksandr Chilikin <majestic905@gmail.com>
2021-10-28 16:55:40 +0300

    Add .tile-centered to Tile

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -9,18 +9,20 @@ import TileSubtitle from './TileSubtitle'
 import TileTitle from './TileTitle'
 
 const propTypes = {
+  centered: PropTypes.bool,
   children: PropTypes.node,
   renderAs: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   className: PropTypes.string
 }
 
 const defaultProps = {
+  centered: false,
   renderAs: 'div'
 }
 
 const Tile = ({ children, ...props }) => {
-  const { className, renderAs: Element, ...attributes } = props
-  const classNames = classnames('tile', className)
+  const { centered, className, renderAs: Element, ...attributes } = props
+  const classNames = classnames('tile', { 'tile-centered': centered }, className)
 
   return (
     <Element {...attributes} className={classNames}>
